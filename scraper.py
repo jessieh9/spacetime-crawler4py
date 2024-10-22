937402f44c59edd7b0e2860f83e84b0bc4ff04e6
Jessie He <jessieh9@circinus-46.ics.uci.edu>
2024-10-21 17:20:30 -0700

    using beutiful soup

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 import re
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
     # Implementation required.
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
@@ -17,6 +19,8 @@ def extract_next_links(url, resp):
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
 
     # test
+    print(soup.find_all('link'))
+    # print(f'url: {url}, resp url: {resp.raw_response.url}, content: {resp.raw_response.content}')
     return list()
 
 def is_valid(url):
